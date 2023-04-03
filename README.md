ff3a3cc41de3c14c3e2bccaeddf8764299494f0b
Alexander Kukushkin <alex@alexkuk.ru>
2023-04-03 12:10:19 +0300

    Init stage. Part eval openai

---
+++ README.md
@@ -0,0 +1,12 @@
+
+Dev env
+
+```bash
+python -m venv ~/.venvs/rulm-sbs
+source ~/.venvs/rulm-sbs/bin/activate
+
+pip install -r requirements.txt
+
+pip install ipykernel
+python -m ipykernel install --user --name rulm-sbs
+```
---
+++ main.py
@@ -0,0 +1,106 @@
+
+import os
+import json
+
+from tqdm import tqdm as log_progress
+
+import pandas as pd
+
+import openai
+
+
+#######
+#
+#   LINES
+#
+######
+
+
+def read_lines(path):
+    with open(path) as file:
+        for line in file:
+            yield line.rstrip('\n')
+
+
+def write_lines(path, lines):
+    with open(path, 'w') as file:
+        for line in lines:
+            file.write(line + '\n')
+
+
+#######
+#
+#   JSONL
+#
+#####
+
+
+def parse_jsonl(lines):
+    for line in lines:
+        yield json.loads(line)
+
+
+def format_jsonl(items):
+    for item in items:
+        yield json.dumps(item, ensure_ascii=False, indent=None)
+
+
+########
+#
+#   DOTENV
+#
+######
+
+
+DOTENV_PATH = '.env'
+
+
+def parse_dotenv(lines):
+    for line in lines:
+        if line:
+            key, value = line.split('=', 1)
+            yield key, value
+
+
+######
+#
+#   PROMPT
+#
+######
+
+
+def user_oriented_openai_prompt(item):
+    if item['input']:
+        return '''Задание: {instruction}
+Ввод: {input}
+
+Ответ: '''.format_map(item)
+    else:
+        return '''Задание: {instruction}
+        
+Ответ: '''.format_map(item)
+
+
+########
+#
+#   OPENAI
+#
+######
+
+
+def openai_complete(prompt, model='text-davinci-003'):
+    completion = openai.Completion.create(
+        model=model,
+        prompt=prompt,
+        max_tokens=1024,
+    )
+    return completion.choices[0].text
+
+
+def openai_chat_complete(prompt, model='gpt-3.5-turbo'):
+    completion = openai.ChatCompletion.create(
+        model=model,
+        messages=[{'role': 'user', 'content': prompt}],
+        max_tokens=1024,
+    )
+    return completion.choices[0].message.content
